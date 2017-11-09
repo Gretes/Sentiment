b291c7779bae74da9a493a981f88e28bf87658d4
raynorax <soybilgen@gmail.com>
2017-11-09 17:50:41 +0300

    Updated Caller

--- 2_TweetCleaners/TweetCaller.py
+++ 2_TweetCleaners/TweetCaller.py
@@ -1,6 +1,14 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 from pymongo import MongoClient
 from pprint import pprint
 import pandas as pd
+from stemizer import getstems
+from string import punctuation
+from nltk import word_tokenize
+import math
+from collections import defaultdict
 
 client = MongoClient('mongodb://veli:kamil7insan@ds237445.mlab.com:37445/heroku_wzpvdrhs')
 db = client['heroku_wzpvdrhs']
@@ -11,8 +19,45 @@ collection = db.thy
 
 
 # Regex query for turkcell
-query = { "text": { "$regex": "/.*kcell.*/"} }
-posts = pd.DataFrame([p for p in collection.find(query).limit(100)])
+#query = { "text": { "$regex": "/.*kcell.*/"} }
+posts = pd.DataFrame([p for p in collection.find().limit(10000)])
 
 # write to json
 posts.to_json("sample.json")
+
+tweets = posts['text']
+# [getstems(t) for t in tweets]
+
+# stop_words  = pd.read_excel("stopwordstr.xlsx").values.tolist()
+# stop_words = stop_words + list(punctuation)
+
+bagofwords = [getstems(t) for t in tweets]
+vocabulary = sum(bagofwords,[])
+vocabulary = list(set(vocabulary))
+
+stemmedtweets = [" ".join(b) for b in bagofwords]
+DOCUMENTS_COUNT = len(stemmedtweets)
+word_idf = defaultdict(lambda: 0)
+
+for words in bagofwords:
+    words = set(words)
+    for word in words:
+        word_idf[word] += 1
+        for word in vocabulary:
+            word_idf[word] = math.log(DOCUMENTS_COUNT / float(1 + word_idf[word]))
+
+#count_vect = CountVectorizer()
+#X_train_counts = count_vect.fit_transform(stemmedtweets)
+#X_train_counts.shape
+#tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
+#X_train_tf = tf_transformer.transform(X_train_counts)
+#keyword=sum(X_train_tf)
+
+tfidf.fit(stemmedtweets[5])
+# Transform a document into TfIdf coordinates
+X = tfidf.transform(stemmedtweets[3:2002])
+# Check out some frequencies
+print(X[0, tfidf.vocabulary_['thy']])                   # 0.0562524229373
+
+for t in tweets:
+    print(t)
